763c22beb01e5ecd649b0d6a213385eca100e2ea
PHJ <legowww@gmail.com>
2023-07-22 14:59:04 +0900

    refactor: SubmissionDto 필드값 추가

--- src/main/java/com/dev/az/model/dto/SubmissionDto.java
+++ src/main/java/com/dev/az/model/dto/SubmissionDto.java
@@ -1,15 +1,20 @@
 package com.dev.az.model.dto;
 
 import com.dev.az.model.ProblemRank;
+import com.dev.az.model.SubmissionResult;
 import com.dev.az.model.entity.Submission;
 
-public record SubmissionDto(long problemId, String title, ProblemRank problemRank) {
+import java.time.LocalDateTime;
+
+public record SubmissionDto(long problemId, String title, ProblemRank problemRank, SubmissionResult submissionResult, LocalDateTime createdAt) {
 
     public static SubmissionDto from(Submission submission) {
         return new SubmissionDto(
                 submission.getProblem().getId(),
                 submission.getProblem().getTitle(),
-                submission.getProblem().getProblemRank()
+                submission.getProblem().getProblemRank(),
+                submission.getSubmissionResult(),
+                submission.getCreatedAt()
         );
     }
 }
